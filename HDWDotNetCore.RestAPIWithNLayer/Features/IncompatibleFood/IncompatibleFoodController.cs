b92bbb0789b3bf58049e438c5751bedc108ffb31
Hnin Daiwee <hnindaiwe468@gmail.com>
2024-05-14 21:31:49 +0800

    API for Incompatible Food

---
+++ HDWDotNetCore.RestAPIWithNLayer/Features/IncompatibleFood/IncompatibleFoodController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static HDWDotNetCore.RestAPIWithNLayer.Models.IncompatibleFoodModel;
+
+namespace HDWDotNetCore.RestAPIWithNLayer.Features.IncompatibleFood
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncompatibleFoodController : ControllerBase
+    {
+        private async Task<Incompatiblefood> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("IncompatibleFood.json");
+            var model = JsonConvert.DeserializeObject<Incompatiblefood>(jsonStr);
+            return model!;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model);
+        }
+        [HttpGet("Categories")]
+        public async Task<IActionResult> Categories()
+        {
+            var model = await GetDataAsync();
+            var categories = model!.Tbl_IncompatibleFood
+                                    .DistinctBy(x => x.Description)
+                                    .Select(x => new IncompatibleFoodCategory()
+                                    {
+                                        Description = x.Description
+                                    }).ToList();
+
+            return Ok(categories);
+        }
+        [HttpGet("SearchByCategory")]
+
+        public async Task<IActionResult> ListByCategory(string category)
+        {
+            var model = await GetDataAsync();
+            var list = model!.Tbl_IncompatibleFood
+                              .Where(x => x.Description == category)
+                              .Select(x => new IncompatibleFoodRespondData()
+                              {
+                                  Id = x.Id,
+                                  FoodA = x.FoodA,
+                                  FoodB = x.FoodB,
+                                  Description = x.Description
+                              }).ToList();
+            return Ok(list);
+        }
+
+    }
+}
+
---
+++ HDWDotNetCore.RestAPIWithNLayer/Models/IncompatibleFoodModel.cs
@@ -0,0 +1,30 @@
+﻿namespace HDWDotNetCore.RestAPIWithNLayer.Models
+{
+    public class IncompatibleFoodModel
+    {
+
+        public class Incompatiblefood
+        {
+            public Tbl_Incompatiblefood[] Tbl_IncompatibleFood { get; set; }
+        }
+
+        public class Tbl_Incompatiblefood
+        {
+            public int Id { get; set; }
+            public string FoodA { get; set; }
+            public string FoodB { get; set; }
+            public string Description { get; set; }
+        }
+        public class IncompatibleFoodRespondData
+        {
+            public int Id { get; set; }
+            public string FoodA { get; set; }
+            public string FoodB { get; set; }
+            public string Description { get; set; }
+        }
+        public class IncompatibleFoodCategory
+        {
+            public string Description { get; set; }
+        }
+    }
+}
